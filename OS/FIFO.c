3be6973fc9edcd2277b55e6034b759c73602b860
Vigneshd705 <vigneshd705@gmail.com>
2023-11-15 08:10:36 +0530

    OS

--- OS/FIFO.c
+++ OS/FIFO.c
@@ -1,48 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
-// FIFO - PAGE REPLACEMENT ALG.
-int check(int arr_f[], int size, int data)
-{
-	int c = 0;
-	for (int i = 0; i < size; i++)
-	{
-		if (data != arr_f[i])
-		{
-			c++;
-		}
-	}
-	if (c == size)
-
-		return 1;
-	return 0;
-}
-
+int page[100], frame[10];
 void main()
 {
-	int frame, page, page_fault = 0;
-	printf("Enter the total frame size : ");
-	scanf("%d", &frame);
-	int arr_f[frame];
-	arr_f[0] = -1;
-	arr_f[1] = -1;
-	arr_f[2] = -1;
-	printf("Page size : ");
-	scanf("%d", &page);
-	int arr_p[page];
-	for (int i = 0; i < page; i++)
-	{
-		printf("Page %d : ", i + 1);
-		scanf("%d", &arr_p[i]);
-	}
-	for (int i = 0; i < page; i++)
-	{
-		if (check(arr_f, frame, arr_p[i]))
-		{
-			arr_f[page_fault % frame] = arr_p[i];
-			page_fault++;
-		}
-	}
-	for (int i = 0; i < frame; i++)
-		printf("%d ", arr_f[i]);
-	printf("\nPAGE FAULT : %d\nPAGE HIT : %d\n", page_fault, page - page_fault);
+    int pages, frames, pagefalut = 0, k = 0;
+    printf("Enter the no of pages: ");
+    scanf("%d", &pages);
+    printf("Start entering the page values: ");
+    for (int i = 0; i < pages; i++)
+    {
+        scanf("%d", &page[i]);
+    }
+    printf("Enter the no. of frames: ");
+    scanf("%d", &frames);
+    for (int i = 0; i < frames; i++)
+    {
+        frame[i] = -1;
+    }
+    for (int i = 0; i < frames; i++)
+    {
+        printf("frame %d\t\t", i + 1);
+    }
+    printf("\n");
+    for (int i = 0; i < pages; i++)
+    {
+        int flag = 0;
+        for (int j = 0; j < frames; j++)
+        {
+            if (frame[j] == page[i])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag != 1)
+        {
+            frame[k % frames] = page[i];
+            k++;
+            pagefalut++;
+        }
+        else
+        {
+            flag = 0;
+        }
+        for (int l = 0; l < frames; l++)
+        {
+            if (frame[l] == -1)
+            {
+                printf("-\t\t");
+            }
+            else
+            {
+                printf("%d\t\t", frame[l]);
+            }
+        }
+        printf("\n");
+    }
+    printf("Total Pagefaults: %d\n",pagefalut);
 }
